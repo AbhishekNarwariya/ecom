a8d5bac79ae0c986df34996edac4e92e9b23ae7f
AbhishekNarwariya <narwariyaabhishek50@gmail.com>
2024-02-11 15:27:06 +0530

    adding authnavigation

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SellerService } from '../services/seller.service';
 import { Router } from '@angular/router';
+import { signUp } from '../data-type';
 
 
 @Component({
@@ -12,18 +13,13 @@ export class SellerAuthComponent {
 
   constructor(private seller: SellerService, private router:Router) {}
 
-  signUp(data:object):void{
+  ngOnInit(): void {
+    this.seller.reloadSeller()
+  }
+
+  signUp(data:signUp):void{
    
-    this.seller.userSignUp(data).subscribe((result)=>{
-      // console.log(result);
-
-      if (result) {
-        this.router.navigate(['seller-home'])
-        
-      }
-      
-
-    })
+    this.seller.userSignUp(data)
     
   }
 
--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -1,18 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { signUp } from '../data-type';
+import { response } from 'express';
+import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SellerService {
 
-  constructor(private http:HttpClient) { }
-  userSignUp(data:any){
-    return this.http.post('http://localhost:3000/seller', data)
-    // {observe:'response'}).subscribe((result)=>{
-      // console.warn(result)
-      // if(result){
-        // localStorage.setItem('seller',JSON.stringify(result.body))
-        // this.router.navigate(['seller-home'])
+  isSellerLoggedIn=new BehaviorSubject<boolean>(false)
+
+  constructor(private http:HttpClient, private router:Router) { }
+  userSignUp(data:signUp){
+
+    this.http.post('http://localhost:3000/seller', data, {observe:'response'})
+    .subscribe((result)=>{
+      this.isSellerLoggedIn.next(true)
+
+      localStorage.setItem('seller', JSON.stringify(result.body))
+      
+      this.router.navigate(['seller-home'])
+     
+      
+    })
+   
+
+  }
+  reloadSeller(){
+    if(localStorage.getItem('seller')){
+      this.isSellerLoggedIn.next(true)
+      this.router.navigate(['seller-home'])
+    }
   }
 }
